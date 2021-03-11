e7197fb6fc606a5ecf87bddd40105b22cf65b59d
budlebee <budlebeee@gmail.com>
2021-03-11 16:09:24 +0900

    Initial Commit

---
+++ Shared/SwiftUI_PDFReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PDFReaderApp.swift
+//  Shared
+//
+//  Created by zowan on 2021/03/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PDFReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
